759d6b3f1bdaf85d37c51649e2ee1a8dc92ef9f5
danielmalancioiu <dmalancioiu@gmail.com>
2024-11-26 00:54:56 +0200

    add stat cards + date picker

---
+++ app/(dashboard)/_components/Overview.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { DateRangePicker } from "@/components/ui/date-range-picker";
+import { MAX_DATE_RANGE_DAYS } from "@/lib/constants";
+import { UserSettings } from "@prisma/client";
+import { differenceInDays, startOfMonth } from "date-fns";
+import React, { useState } from "react";
+import { toast } from "sonner";
+import StatsCards from "./StatsCards";
+
+function Overview({ userSettings }: { userSettings: UserSettings }) {
+  const [dateRange, setDateRange] = useState<{ from: Date; to: Date }>({
+    from: startOfMonth(new Date()),
+    to: new Date(),
+  });
+  return (
+    <>
+      <div className="w-full flex flex-wrap items-end justify-between gap-2 py-6 px-6">
+        <h2 className="text-3xl font-bold">Overview</h2>
+        <div className="flex items-center gap-3">
+          <DateRangePicker
+            initialDateFrom={dateRange.from}
+            initialDateTo={dateRange.to}
+            showCompare={false}
+            onUpdate={(values) => {
+              const { from, to } = values.range;
+
+              if (!from || !to) return;
+              if (differenceInDays(to, from) > MAX_DATE_RANGE_DAYS) {
+                toast.error(
+                  `The selcted date range is too big. Max allowed rnage is ${MAX_DATE_RANGE_DAYS} days!`
+                );
+                return;
+              }
+
+              setDateRange({ from, to });
+            }}
+          />
+        </div>
+      </div>
+      <div className="w-full flex flex-col gap-2 px-6 py-6">
+        <StatsCards
+          userSettings={userSettings}
+          from={dateRange.from}
+          to={dateRange.to}
+        />
+      </div>
+    </>
+  );
+}
+
+export default Overview;
